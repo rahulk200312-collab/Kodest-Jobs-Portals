1cf70725e6082e91961b90554f22598e82fa8d97
rahulk200312-collab <rahulk200312@gmail.com>
2026-03-24 19:10:33 +0530

    Initialize script.js with API and DSA data

---
+++ script.js
@@ -0,0 +1,14 @@
+const API_URL = 'http://localhost:8080/api';
+
+let jobData = [];
+let companyData = [];
+let liveJobs = [];
+let isLiveMode = false;
+
+const dsaSheet = [
+  { topic: "Arrays", problem: "Two Sum", difficulty: "Easy", link: "dsa-problems/Arrays/two-sum.html" },
+  { topic: "Arrays", problem: "Best Time to Buy and Sell Stock", difficulty: "Easy", link: "dsa-problems/Arrays/best-time-to-buy-and-sell-stock.html" },
+  { topic: "Linked List", problem: "Reverse Linked List", difficulty: "Easy", link: "dsa-problems/Linkedlist/reverse-linked-list.html" },
+  { topic: "Trees", problem: "Binary Tree Inorder Traversal", difficulty: "Easy", link: "dsa-problems/Trees/binary-tree-inorder-traversal.html" },
+  { topic: "Graph", problem: "Number of Islands", difficulty: "Medium", link: "dsa-problems/Graph/number-of-islands.html" },
+  { topic: "Stringscrip
